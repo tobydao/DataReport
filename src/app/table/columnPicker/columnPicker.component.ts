005b06175d46cb14ca1c33e0d4d37ed2d1688a64
tobydao <tob.dao@gmail.com>
2022-07-29 14:27:04 +0200

    working filter

---
+++ src/app/table/columnPicker/columnPicker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-columnPicker',
+  templateUrl: './columnPicker.component.html',
+  styleUrls: [ './columnPicker.component.css' ]
+})
+export class ColumnPickerComponent {
+
+}
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -9,23 +9,45 @@ import { JSONService } from '../shared/json.service';
 })
 
 export class TableComponent implements OnInit{
-  data = [];
+  dataToDisplay = []
   displayedColumns = [];
   columnsToDisplay = [];
   dataSource = new MatTableDataSource<Object>();
 
   constructor(private jsonService: JSONService) {
     this.jsonService.getJSON().subscribe(json => {
-      this.data = json;
-      this.displayedColumns = Object.keys(this.data[0])
+      this.dataSource.data = json;
+      this.displayedColumns = Object.keys(this.dataSource.data[0]);
       this.columnsToDisplay = this.displayedColumns.slice();
-      this.dataSource.data = this.data;
+      this.dataToDisplay = this.dataSource.data.slice();
     })
   }
 
   ngOnInit(){
   }
-  
+
+  //ngOnDestroy unsubscribe
+
+  //row operations
+
+
+  //column operations
+  addColumn() {
+    const randomColumn = Math.floor(Math.random() * this.displayedColumns.length);
+    this.columnsToDisplay.push(this.displayedColumns[randomColumn]);
+  }
+
+  removeColumn() {
+    if (this.columnsToDisplay.length) {
+      this.columnsToDisplay.pop();
+    }
+  }
+  resetTable() {
+    if(this.displayedColumns.length){
+      this.columnsToDisplay = this.displayedColumns.slice();
+    }
+  }
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
